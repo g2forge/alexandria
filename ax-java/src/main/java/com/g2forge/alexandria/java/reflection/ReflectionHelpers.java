2b59b74abd31ac5c920674c1565960c7e05156d8
Greg Gibeling <gdgib@outlook.com>
2016-04-07 19:35:05 -0700

    Added reflection helper support for methods

--- ax-java/src/main/java/com/g2forge/alexandria/java/reflection/ReflectionHelpers.java
+++ ax-java/src/main/java/com/g2forge/alexandria/java/reflection/ReflectionHelpers.java
@@ -1,6 +1,8 @@
 package com.g2forge.alexandria.java.reflection;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
 import java.util.Collections;
@@ -9,11 +11,21 @@ import java.util.stream.Stream;
 import com.g2forge.alexandria.java.TreeHelpers;
 
 public class ReflectionHelpers {
-	public static Stream<Field> getFields(Class<?> klass, JavaScope scope, JavaProtection minimum) {
-		final Stream<Class<?>> classes = scope.isInherited() ? TreeHelpers.<Class<?>>dfs(klass, c -> Object.class.equals(c) ? Collections.emptyList() : Arrays.<Class<?>>asList(c.getSuperclass()), true) : Stream.of(klass);
-		final Stream<Field> fields = classes.map(Class::getDeclaredFields).flatMap(Stream::of);
-		final Stream<Field> scoped = fields.filter(field -> !(scope.isStatics() ^ Modifier.isStatic(field.getModifiers())));
-		final Stream<Field> filtered = minimum == null ? scoped : scoped.filter(field -> JavaProtection.of(field).compareTo(minimum) >= 0);
+	protected static <T extends Member> Stream<T> filter(JavaScope scope, JavaProtection minimum, final Stream<T> members) {
+		final Stream<T> scoped = members.filter(member -> !(scope.isStatics() ^ Modifier.isStatic(member.getModifiers())));
+		final Stream<T> filtered = minimum == null ? scoped : scoped.filter(member -> JavaProtection.of(member).compareTo(minimum) >= 0);
 		return filtered;
+	}
+
+	protected static Stream<Class<?>> getClasses(Class<?> klass, JavaScope scope) {
+		return scope.isInherited() ? TreeHelpers.<Class<?>>dfs(klass, c -> Object.class.equals(c) ? Collections.emptyList() : Arrays.<Class<?>>asList(c.getSuperclass()), true) : Stream.of(klass);
+	}
+
+	public static Stream<Field> getFields(Class<?> klass, JavaScope scope, JavaProtection minimum) {
+		return filter(scope, minimum, getClasses(klass, scope).map(Class::getDeclaredFields).flatMap(Stream::of));
+	}
+
+	public static Stream<Method> getMethods(Class<?> klass, JavaScope scope, JavaProtection minimum) {
+		return filter(scope, minimum, getClasses(klass, scope).map(Class::getDeclaredMethods).flatMap(Stream::of));
 	}
 }
