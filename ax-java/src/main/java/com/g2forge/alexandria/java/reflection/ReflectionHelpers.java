dcb1e1bc4c27d5a20fa05cbd29ebf454f7ed620e
Greg Gibeling <gdgib@outlook.com>
2016-04-08 09:13:32 -0700

    Filter out overriden methods

--- ax-java/src/main/java/com/g2forge/alexandria/java/reflection/ReflectionHelpers.java
+++ ax-java/src/main/java/com/g2forge/alexandria/java/reflection/ReflectionHelpers.java
@@ -4,13 +4,38 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Member;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
+import com.g2forge.alexandria.java.MapHelpers;
 import com.g2forge.alexandria.java.TreeHelpers;
 
 public class ReflectionHelpers {
+	protected static class OverrideFilter implements Predicate<Method> {
+		protected final Map<String, Collection<Method>> map = new HashMap<>();
+
+		@Override
+		public boolean test(Method method) {
+			final Collection<Method> methods = MapHelpers.createOrGet(map, method.getName(), name -> new ArrayList<>());
+			METHOD: for (Method prior : methods) {
+				if (method.getParameterCount() != prior.getParameterCount()) continue;
+				if (!prior.getReturnType().isAssignableFrom(method.getReturnType())) continue;
+				for (int i = 0; i < method.getParameterCount(); i++) {
+					if (!method.getParameterTypes()[i].isAssignableFrom(prior.getParameterTypes()[i])) continue METHOD;
+				}
+				return false;
+			}
+			methods.add(method);
+			return true;
+		}
+	}
+
 	protected static <T extends Member> Stream<T> filter(JavaScope scope, JavaProtection minimum, final Stream<T> members) {
 		final Stream<T> scoped = members.filter(member -> !(scope.isStatics() ^ Modifier.isStatic(member.getModifiers())));
 		final Stream<T> filtered = minimum == null ? scoped : scoped.filter(member -> JavaProtection.of(member).compareTo(minimum) >= 0);
@@ -26,6 +51,7 @@ public class ReflectionHelpers {
 	}
 
 	public static Stream<Method> getMethods(Class<?> klass, JavaScope scope, JavaProtection minimum) {
-		return filter(scope, minimum, getClasses(klass, scope).map(Class::getDeclaredMethods).flatMap(Stream::of));
+		final Stream<Method> basic = filter(scope, minimum, getClasses(klass, scope).map(Class::getDeclaredMethods).flatMap(Stream::of));
+		return basic.sequential().filter(new OverrideFilter());
 	}
 }
