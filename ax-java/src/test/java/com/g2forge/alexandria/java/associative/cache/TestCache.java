fa197d7bb63ebdbbf6eb7e420fbd375c02989e45
Greg Gibeling <gdgib@outlook.com>
2016-04-09 08:34:48 -0700

    Implemented Caches w/Pluggable Eviction Policies

---
+++ ax-java/src/test/java/com/g2forge/alexandria/java/associative/cache/TestCache.java
@@ -0,0 +1,32 @@
+package com.g2forge.alexandria.java.associative.cache;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.g2forge.alexandria.java.function.RecordingFunction;
+import com.g2forge.alexandria.java.tuple.ITuple2G_;
+
+public class TestCache {
+	protected final RecordingFunction<Integer, Integer> function = new RecordingFunction<>(x -> x + 1);
+
+	protected final Cache<Integer, Integer> cache = new Cache<Integer, Integer>(function, new LRUCacheEvictionPolicy<>(1));
+
+	@Test
+	public void cache() {
+		Assert.assertEquals(1, cache.apply(0).intValue());
+		Assert.assertEquals(1, cache.apply(0).intValue());
+		Assert.assertEquals(Arrays.asList(0), function.getRecord().stream().map(ITuple2G_::get0).collect(Collectors.toList()));
+	}
+
+	@Test
+	public void evict() {
+		Assert.assertEquals(1, cache.apply(0).intValue());
+		Assert.assertEquals(2, cache.apply(1).intValue());
+		Assert.assertEquals(Arrays.asList(0, 1), function.getRecord().stream().map(ITuple2G_::get0).collect(Collectors.toList()));
+		Assert.assertEquals(1, cache.apply(0).intValue());
+		Assert.assertEquals(Arrays.asList(0, 1, 0), function.getRecord().stream().map(ITuple2G_::get0).collect(Collectors.toList()));
+	}
+}
