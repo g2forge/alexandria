b061f1c0f491ea58d5077d8ed623a9de0e575b50
Greg Gibeling <gdgib@outlook.com>
2016-04-18 21:02:24 -0700

    Tuple identities

--- ax-java/src/main/java/com/g2forge/alexandria/java/tuple/implementations/Tuple2G_.java
+++ ax-java/src/main/java/com/g2forge/alexandria/java/tuple/implementations/Tuple2G_.java
@@ -2,19 +2,16 @@ package com.g2forge.alexandria.java.tuple.implementations;
 
 import java.util.Objects;
 
-import com.g2forge.alexandria.java.tuple.ITuple2GS;
+import com.g2forge.alexandria.java.tuple.ITuple2G_;
 
-public class Tuple2GS<T0, T1> extends Tuple1GS<T0>implements ITuple2GS<T0, T1> {
-	protected T1 value1;
-
-	public Tuple2GS() {
-		this(null, null);
-	}
+public class Tuple2G_<T0, T1> extends Tuple1G_<T0>implements ITuple2G_<T0, T1> {
+	protected final T1 value1;
 
 	/**
+	 * @param value0
 	 * @param value1
 	 */
-	public Tuple2GS(final T0 value0, final T1 value1) {
+	public Tuple2G_(final T0 value0, final T1 value1) {
 		super(value0);
 		this.value1 = value1;
 	}
@@ -37,24 +34,5 @@ public class Tuple2GS<T0, T1> extends Tuple1GS<T0>implements ITuple2GS<T0, T1> {
 	@Override
 	public int hashCode() {
 		return Objects.hash(get0(), get1());
-	}
-
-	@Override
-	public ITuple2GS<T0, T1> set0(final T0 value) {
-		value0 = value;
-		return this;
-	}
-
-	@Override
-	public ITuple2GS<T0, T1> set1(final T1 value) {
-		this.value1 = value;
-		return this;
-	}
-
-	@Override
-	public T1 swap1(final T1 value) {
-		final T1 retVal = value1;
-		set1(value);
-		return retVal;
 	}
 }
