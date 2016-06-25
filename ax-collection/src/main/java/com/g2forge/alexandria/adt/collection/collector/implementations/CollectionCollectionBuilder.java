28ea6b6ccfe019b24f9ddd284a8cc0e459d86003
Greg Gibeling <gdgib@outlook.com>
2016-06-25 09:14:38 -0700

    Minor improvements to collection builders

--- ax-collection/src/main/java/com/g2forge/alexandria/adt/collection/collector/implementations/CollectionCollectionBuilder.java
+++ ax-collection/src/main/java/com/g2forge/alexandria/adt/collection/collector/implementations/CollectionCollectionBuilder.java
@@ -3,20 +3,19 @@ package com.g2forge.alexandria.adt.collection.collector.implementations;
 import java.util.Collection;
 
 import com.g2forge.alexandria.adt.collection.collector.ICollectionBuilder;
-import com.g2forge.alexandria.adt.collection.collector.ICollector;
 
-public class CollectionCollector<C extends Collection<? super T>, T> implements ICollectionBuilder<C, T> {
+public class CollectionCollectionBuilder<C extends Collection<? super T>, T> implements ICollectionBuilder<C, T> {
 	protected final C collection;
 
 	/**
 	 * @param collection
 	 */
-	public CollectionCollector(C collection) {
+	public CollectionCollectionBuilder(C collection) {
 		this.collection = collection;
 	}
 
 	@Override
-	public ICollector<T> add(Iterable<? extends T> values) {
+	public ICollectionBuilder<C, T> add(Iterable<? extends T> values) {
 		for (T value : values) {
 			collection.add(value);
 		}
@@ -24,13 +23,13 @@ public class CollectionCollector<C extends Collection<? super T>, T> implements 
 	}
 
 	@Override
-	public ICollector<T> add(T value) {
+	public ICollectionBuilder<C, T> add(T value) {
 		collection.add(value);
 		return this;
 	}
 
 	@Override
-	public ICollector<T> add(@SuppressWarnings("unchecked") T... values) {
+	public ICollectionBuilder<C, T> add(@SuppressWarnings("unchecked") T... values) {
 		for (T value : values) {
 			collection.add(value);
 		}
