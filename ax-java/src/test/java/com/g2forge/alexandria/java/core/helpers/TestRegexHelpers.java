89accc8189e4b1fd4f6b6031f08ab5978f0669b2
Greg Gibeling <gdgib@outlook.com>
2016-09-02 07:28:29 -0700

    ToList Regex Helper

---
+++ ax-java/src/test/java/com/g2forge/alexandria/java/core/helpers/TestRegexHelpers.java
@@ -0,0 +1,18 @@
+package com.g2forge.alexandria.java.core.helpers;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestRegexHelpers {
+	@Test
+	public void list() {
+		final String regex = "(" + RegexHelpers.toList("[a-z]+", ",\\s*") + ")(_foo)";
+		final Matcher matcher = Pattern.compile(regex).matcher("one, two_foo");
+		Assert.assertTrue(matcher.matches());
+		Assert.assertEquals("one, two", matcher.group(1));
+		Assert.assertEquals("_foo", matcher.group(2));
+	}
+}
