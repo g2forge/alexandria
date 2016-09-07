5b548ebd268ac98d25fe64fe3ea7229b861e4073
Greg Gibeling <gdgib@outlook.com>
2016-09-06 22:42:47 -0700

    OptionalHelpers.upcast() method

---
+++ ax-java/src/main/java/com/g2forge/alexandria/java/fluent/optional/OptionalHelpers.java
@@ -0,0 +1,9 @@
+package com.g2forge.alexandria.java.fluent.optional;
+
+import java.util.Optional;
+
+public class OptionalHelpers {
+	public static <T> Optional<T> upcast(Optional<? extends T> optional) {
+		return Optional.ofNullable(optional.orElseGet(null));
+	}
+}
