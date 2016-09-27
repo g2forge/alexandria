6aacf2edc4976219520c27aa48f1d06c413e42eb
Greg Gibeling <gdgib@outlook.com>
2016-09-26 23:39:49 -0700

    Supplier toFunction()

--- ax-java/src/main/java/com/g2forge/alexandria/java/function/ISupplier.java
+++ ax-java/src/main/java/com/g2forge/alexandria/java/function/ISupplier.java
@@ -3,4 +3,8 @@ package com.g2forge.alexandria.java.function;
 import java.util.function.Supplier;
 
 @FunctionalInterface
-public interface ISupplier<T> extends Supplier<T> {}
+public interface ISupplier<T> extends Supplier<T> {
+	public default <I> IFunction<I, T> toFunction() {
+		return t -> get();
+	}
+}
