51b63c6fa24036281cb8a2d8261e257b12ce53c5
Greg Gibeling <gdgib@outlook.com>
2016-10-04 08:55:38 -0700

    Unit test

---
+++ ax-java/src/test/java/com/g2forge/alexandria/java/fluent/optional/TestIOptionalFunction.java
@@ -0,0 +1,16 @@
+package com.g2forge.alexandria.java.fluent.optional;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.g2forge.alexandria.java.fluent.optional.function.IOptionalFunction;
+
+public class TestIOptionalFunction {
+	@Test
+	public void override() {
+		final IOptionalFunction<Object, String> override = IOptionalFunction.of(NullableOptional.FACTORY, 0, "Zero").override(IOptionalFunction.of(NullableOptional.FACTORY, 1, "One"));
+		Assert.assertEquals("Zero", override.apply(0).get());
+		Assert.assertEquals("One", override.apply(1).get());
+		Assert.assertTrue(override.apply(2).isEmpty());
+	}
+}
