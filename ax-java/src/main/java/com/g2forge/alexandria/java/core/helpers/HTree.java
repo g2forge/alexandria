b4760ffa217d8f3fb5c64bc84b83124b934fb99c
Greg Gibeling <gdgib@outlook.com>
2016-10-09 17:34:18 -0700

    HTree.find

--- ax-java/src/main/java/com/g2forge/alexandria/java/core/helpers/HTree.java
+++ ax-java/src/main/java/com/g2forge/alexandria/java/core/helpers/HTree.java
@@ -1,7 +1,9 @@
 package com.g2forge.alexandria.java.core.helpers;
 
 import java.util.Collection;
+import java.util.Optional;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 import com.g2forge.alexandria.java.marker.Helpers;
@@ -11,9 +13,18 @@ import lombok.experimental.UtilityClass;
 @Helpers
 @UtilityClass
 public class HTree {
-	public static <N> Stream<N> dfs(N node, final Function<N, Collection<N>> getChildren, boolean postorder) {
-		final Collection<N> children = getChildren.apply(node);
+	public static <N> Stream<N> dfs(N node, final Function<? super N, ? extends Collection<? extends N>> getChildren, boolean postorder) {
+		final Collection<? extends N> children = getChildren.apply(node);
 		if ((children == null) || children.isEmpty()) return Stream.of(node);
 		else return children.stream().map(child -> dfs(child, getChildren, postorder)).reduce(Stream.of(node), postorder ? (s0, s1) -> Stream.concat(s1, s0) : Stream::concat);
+	}
+
+	public static <N> Optional<N> find(N node, final Function<? super N, ? extends Collection<? extends N>> getChildren, Predicate<? super N> find) {
+		if (find.test(node)) return Optional.of(node);
+		for (N child : getChildren.apply(node)) {
+			final Optional<N> retVal = find(child, getChildren, find);
+			if (retVal.isPresent()) return retVal;
+		}
+		return Optional.empty();
 	}
 }
