30d0a68a88616c7ab36b9a1948496e14f976f398
Greg Gibeling <gdgib@outlook.com>
2019-01-19 10:00:38 -0800

    G2-304 Added proper andThen overrides to IConsumerX interfaces

--- ax-java/src/main/java/com/g2forge/alexandria/java/function/IConsumer2.java
+++ ax-java/src/main/java/com/g2forge/alexandria/java/function/IConsumer2.java
@@ -1,11 +1,20 @@
 package com.g2forge.alexandria.java.function;
 
+import java.util.Objects;
 import java.util.function.BiConsumer;
 
 @FunctionalInterface
 public interface IConsumer2<I0, I1> extends BiConsumer<I0, I1>, IConsumer {
 	public static <I0, I1> IConsumer2<I0, I1> create(IConsumer2<I0, I1> consumer) {
 		return consumer;
+	}
+
+	public default IConsumer2<I0, I1> andThen(BiConsumer<? super I0, ? super I1> after) {
+		Objects.requireNonNull(after);
+		return (input0, input1) -> {
+			accept(input0, input1);
+			after.accept(input0, input1);
+		};
 	}
 
 	public default IConsumer1<I1> curry0(I0 input0) {
