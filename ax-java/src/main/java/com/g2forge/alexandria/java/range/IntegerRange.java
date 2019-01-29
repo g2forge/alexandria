303fc49c16f9f29d0de5cd95f09a9d4ecfc1d8df
Greg Gibeling <gdgib@outlook.com>
2019-01-28 23:04:16 -0800

    G2-308 Integer Ranges

---
+++ ax-java/src/main/java/com/g2forge/alexandria/java/range/IntegerRange.java
@@ -0,0 +1,36 @@
+package com.g2forge.alexandria.java.range;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@EqualsAndHashCode
+@ToString
+public class IntegerRange implements IRange<Integer> {
+	protected final int min;
+
+	protected final int max;
+
+	public IntegerRange(int min, int max) {
+		this.min = min;
+		this.max = max;
+	}
+
+	@Override
+	public Integer getMax() {
+		return max;
+	}
+
+	@Override
+	public Integer getMin() {
+		return min;
+	}
+
+	public IntegerRange wrapSubRange(IRange<Integer> subrange) {
+		final int delta = getMax() - (getMin()) + 1;
+		final int min = subrange.getMin() < 0 ? subrange.getMin() + delta : subrange.getMin();
+		final int max = subrange.getMax() < 0 ? subrange.getMax() + delta : subrange.getMax();
+		final IntegerRange retVal = new IntegerRange(min, max);
+		validateSubRange(retVal);
+		return retVal;
+	}
+}
