38d28272b8fdd052b4988837e20451f9a23ee937
Greg Gibeling <gdgib@outlook.com>
2019-03-08 08:30:10 -0800

    G2-325 Temporary log levels

--- ax-log-api/src/main/java/com/g2forge/alexandria/log/ILogControl.java
+++ ax-log-api/src/main/java/com/g2forge/alexandria/log/ILogControl.java
@@ -2,6 +2,16 @@ package com.g2forge.alexandria.log;
 
 import org.slf4j.event.Level;
 
+import com.g2forge.alexandria.java.close.ICloseable;
+
 public interface ILogControl {
+	public Level getLogLevel();
+
 	public void setLogLevel(Level level);
+
+	public default ICloseable tempLogLevel(Level level) {
+		final Level original = getLogLevel();
+		setLogLevel(level);
+		return () -> setLogLevel(original);
+	}
 }
