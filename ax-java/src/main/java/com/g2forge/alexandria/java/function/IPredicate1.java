30a4bedc61803cb7d2c3ee1c0a14813a034d3056
Greg Gibeling <gdgib@outlook.com>
2019-03-26 22:44:12 -0700

    G2-325 Minor predicate enhancements

--- ax-java/src/main/java/com/g2forge/alexandria/java/function/IPredicate1.java
+++ ax-java/src/main/java/com/g2forge/alexandria/java/function/IPredicate1.java
@@ -11,4 +11,16 @@ public interface IPredicate1<T> extends Predicate<T>, IPredicate {
 	public static <T> IPredicate1<T> create(IPredicate1<T> predicate) {
 		return predicate;
 	}
+
+	public default <IX> IPredicate2<IX, T> ignore0() {
+		return (i0, i1) -> test(i1);
+	}
+
+	public default <IX> IPredicate2<T, IX> ignore1() {
+		return (i0, i1) -> test(i0);
+	}
+
+	public default IPredicate1<T> negate() {
+		return i -> !test(i);
+	}
 }
