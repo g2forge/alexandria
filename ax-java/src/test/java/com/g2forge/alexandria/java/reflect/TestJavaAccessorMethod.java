f76594cd95405bb99ccadd8e0e99a98b60cc94f6
Greg Gibeling <gdgib@outlook.com>
2019-06-11 11:36:21 -0700

    G2-439 Misc improvements & unit tests

---
+++ ax-java/src/test/java/com/g2forge/alexandria/java/reflect/TestJavaAccessorMethod.java
@@ -0,0 +1,27 @@
+package com.g2forge.alexandria.java.reflect;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import lombok.Data;
+
+public class TestJavaAccessorMethod {
+	@Data
+	public static class TestData {
+		protected final String string;
+
+		protected final boolean flag;
+	}
+
+	@Test
+	public void flag() throws NoSuchMethodException, SecurityException {
+		final JavaAccessorMethod method = new JavaAccessorMethod(TestData.class.getDeclaredMethod("isFlag"));
+		Assert.assertEquals(IJavaAccessorMethod.Accessor.GET, method.getAccessorType());
+	}
+
+	@Test
+	public void string() throws NoSuchMethodException, SecurityException {
+		final JavaAccessorMethod method = new JavaAccessorMethod(TestData.class.getDeclaredMethod("getString"));
+		Assert.assertEquals(IJavaAccessorMethod.Accessor.GET, method.getAccessorType());
+	}
+}
