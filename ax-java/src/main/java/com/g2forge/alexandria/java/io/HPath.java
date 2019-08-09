e9e764aafd8f53bded2c8199c4e18f175447e78b
Greg Gibeling <gdgib@outlook.com>
2019-08-09 07:55:05 -0700

    G2-602 Helper for splitting file names & extensions

--- ax-java/src/main/java/com/g2forge/alexandria/java/io/HPath.java
+++ ax-java/src/main/java/com/g2forge/alexandria/java/io/HPath.java
@@ -2,6 +2,8 @@ package com.g2forge.alexandria.java.io;
 
 import java.nio.file.Path;
 
+import com.g2forge.alexandria.java.adt.tuple.ITuple2G_;
+import com.g2forge.alexandria.java.adt.tuple.implementations.Tuple2G_O;
 import com.g2forge.alexandria.java.core.marker.Helpers;
 
 import lombok.experimental.UtilityClass;
@@ -20,5 +22,11 @@ public class HPath {
 		final int index = string.indexOf('.');
 		if (index < 0) return "";
 		return string.substring(index);
+	}
+
+	public ITuple2G_<String, String> splitFilename(String filename) {
+		final int index = filename.indexOf('.');
+		if (index < 0) return new Tuple2G_O<>(filename, "");
+		return new Tuple2G_O<>(filename.substring(0, index), filename.substring(index));
 	}
 }
