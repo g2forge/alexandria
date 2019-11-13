2f4a1e2319f4aebd08efbcd84edd88f22e1fa036
Greg Gibeling <gdgib@outlook.com>
2019-11-12 19:49:40 -0800

    G2-646 Deterministic topological sort

--- ax-adt/src/test/java/com/g2forge/alexandria/adt/graph/TestHGraph.java
+++ ax-adt/src/test/java/com/g2forge/alexandria/adt/graph/TestHGraph.java
@@ -3,13 +3,21 @@ package com.g2forge.alexandria.adt.graph;
 import org.junit.Assert;
 import org.junit.Test;
 
-import com.g2forge.alexandria.adt.graph.HGraph;
 import com.g2forge.alexandria.java.core.helpers.HCollection;
 
 public class TestHGraph {
 	@Test(expected = IllegalArgumentException.class)
 	public void cyclic() {
 		HGraph.toposort(HCollection.asList("A", "B"), n -> HCollection.asSet(new String(new char[] { (char) (('B' - n.charAt(0)) + 'A') })), true);
+	}
+
+	@Test
+	public void deterministic() {
+		for (int i = 0; i < 100; i++) {
+			final Object a = new Object();
+			final Object b = new Object();
+			Assert.assertEquals(HCollection.asList(a, b), HGraph.toposort(HCollection.asList(a, b), n -> HCollection.emptySet(), true));
+		}
 	}
 
 	@Test
