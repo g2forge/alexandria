49532c2588c9c724ab80a92d3fbb87cd40fb305f
Greg Gibeling <gdgib@outlook.com>
2019-12-25 17:10:18 -0800

    G2-589 Removed old ax-metadata

--- ax-metadata/src/main/java/com/g2forge/alexandria/metadata/MetadataLoader.java
+++
@@ -1,9 +0,0 @@
-package com.g2forge.alexandria.metadata;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface MetadataLoader {
-	public Class<? extends IMetadataLoader> value();
-}
--- ax-metadata/src/main/java/com/g2forge/alexandria/metadata/annotation/IJavaAnnotated.java
+++
@@ -1,7 +0,0 @@
-package com.g2forge.alexandria.metadata.annotation;
-
-import com.g2forge.alexandria.metadata.annotation.IJavaAnnotations;
-
-public interface IJavaAnnotated {
-	public IJavaAnnotations getAnnotations();
-}
