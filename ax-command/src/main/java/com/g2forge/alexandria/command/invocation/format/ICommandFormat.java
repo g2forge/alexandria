2cb6a5c44a9b76bd94132b24276767b862daa8d4
Greg Gibeling <gdgib@outlook.com>
2020-04-26 09:46:50 -0700

    G2-791 Javadoc improvements

--- ax-command/src/main/java/com/g2forge/alexandria/command/invocation/format/ICommandFormat.java
+++ ax-command/src/main/java/com/g2forge/alexandria/command/invocation/format/ICommandFormat.java
@@ -1,10 +1,10 @@
 package com.g2forge.alexandria.command.invocation.format;
 
 /**
- * A per-command interface to describe how arguments to this command should be quoted by {@link ICommandLineBuilder}. This is per-command because while under
- * POSIX processes are given a pre-parsed command block, but the
- * <a href="https://docs.microsoft.com/en-us/cpp/cpp/main-function-command-line-args?redirectedfrom=MSDN&view=vs-2019">Win32 API</a> specifies that processes
- * get a command line which they are at their leisure to parse any way they like.
+ * A per-command interface to describe how arguments to this command should be quoted by
+ * {@link com.g2forge.alexandria.command.process.cmdline.ICommandLineBuilder}. This is per-command because while under POSIX processes are given a pre-parsed
+ * command block, but the <a href="https://docs.microsoft.com/en-us/cpp/cpp/main-function-command-line-args?redirectedfrom=MSDN&view=vs-2019">Win32 API</a>
+ * specifies that processes get a command line which they are at their leisure to parse any way they like.
  * 
  * If you don't know what command format to use we suggest {@link #getDefault()} as most commands, even on Win32, use the same format due to the Microsoft CRT0
  * code and the sh shell both using it. If the command in question is one from the DOS era (like {@code cmd.exe}) then you may need to use
