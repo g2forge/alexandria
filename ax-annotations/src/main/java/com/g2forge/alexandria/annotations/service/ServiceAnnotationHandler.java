f5d80620d25c35fdd489ad53a445406393b4bcbc
Greg Gibeling <gdgib@outlook.com>
2024-04-06 08:51:09 -0700

    G2-1554 Standardize debugging

--- ax-annotations/src/main/java/com/g2forge/alexandria/annotations/service/ServiceAnnotationHandler.java
+++ ax-annotations/src/main/java/com/g2forge/alexandria/annotations/service/ServiceAnnotationHandler.java
@@ -15,6 +15,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
+import java.util.function.Supplier;
 
 import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
@@ -35,6 +36,10 @@ import com.g2forge.alexandria.annotations.IAnnotationHandler;
 public class ServiceAnnotationHandler implements IAnnotationHandler<Service> {
 	protected static boolean ENABLE_DEBUGGING = false;
 	
+	protected static void debug(ProcessingEnvironment processingEnvironment, Supplier<String> message) {
+		if (ENABLE_DEBUGGING) processingEnvironment.getMessager().printMessage(Diagnostic.Kind.WARNING, message.get());
+	}
+	
 	protected static final Location[] READ_LOCATIONS = new Location[] { StandardLocation.CLASS_OUTPUT, StandardLocation.SOURCE_OUTPUT, StandardLocation.CLASS_PATH, StandardLocation.SOURCE_PATH };
 
 	protected static Set<String> readImplementations(ProcessingEnvironment processingEnvironment, final String fileName, final Location location) throws IOException {
@@ -43,7 +48,7 @@ public class ServiceAnnotationHandler implements IAnnotationHandler<Service> {
 		try {
 			inputResource = processingEnvironment.getFiler().getResource(location, "", fileName);
 		} catch (FileNotFoundException | IllegalArgumentException exception) {
-			if (ENABLE_DEBUGGING) processingEnvironment.getMessager().printMessage(Diagnostic.Kind.WARNING, HAnnotationProcessor.toString(exception));
+			debug(processingEnvironment, () -> HAnnotationProcessor.toString(exception));
 			return Collections.emptySet();
 		}
 
@@ -54,26 +59,20 @@ public class ServiceAnnotationHandler implements IAnnotationHandler<Service> {
 			}
 			return retVal;
 		} catch (FileNotFoundException | NoSuchFileException exception) {
-			if (ENABLE_DEBUGGING) processingEnvironment.getMessager().printMessage(Diagnostic.Kind.WARNING, HAnnotationProcessor.toString(exception));
+			debug(processingEnvironment, () -> HAnnotationProcessor.toString(exception));
 			return Collections.emptySet();
 		} catch (IOException exception) {
-			if (ENABLE_DEBUGGING) processingEnvironment.getMessager().printMessage(Diagnostic.Kind.WARNING, HAnnotationProcessor.toString(exception));
+			debug(processingEnvironment, () -> HAnnotationProcessor.toString(exception));
 			if (exception.getMessage().contains("does not exist")) return Collections.emptySet();
 			throw exception;
 		}
 	}
 
 	protected static void writeImplementations(final ProcessingEnvironment processingEnvironment, final StandardLocation location, final String fileName, Map<String, Element> implementations) throws IOException {
-		if (implementations.isEmpty()) {
-			if (ENABLE_DEBUGGING) processingEnvironment.getMessager().printMessage(Diagnostic.Kind.WARNING, "Deleting " + fileName + " from " + location);
-			processingEnvironment.getFiler().getResource(location, "", fileName).delete();
-		}
-		else {
-			if (ENABLE_DEBUGGING) processingEnvironment.getMessager().printMessage(Diagnostic.Kind.WARNING, "Writing " + fileName + " to " + location);
-			final FileObject outputResource = processingEnvironment.getFiler().createResource(location, "", fileName, implementations.values().toArray(new Element[0]));
-			try (final PrintStream outputStream = new PrintStream(outputResource.openOutputStream())) {
-				implementations.keySet().forEach(outputStream::println);
-			}
+		debug(processingEnvironment, () -> "Writing " + fileName + " to " + location);
+		final FileObject outputResource = processingEnvironment.getFiler().createResource(location, "", fileName, implementations.values().toArray(Element[]::new));
+		try (final PrintStream outputStream = new PrintStream(outputResource.openOutputStream())) {
+			implementations.keySet().forEach(outputStream::println);
 		}
 	}
 
@@ -84,17 +83,21 @@ public class ServiceAnnotationHandler implements IAnnotationHandler<Service> {
 			final String fileName = "META-INF/services/" + entry.getKey();
 			
 			try {
-				Set<String> existing = null;
-				final Elements elements = processingEnvironment.getElementUtils();
+				final Set<String> existing;
 				final Map<String, Element> implementations = new TreeMap<>();
-				for (Location location : READ_LOCATIONS) {
-					final Set<String> loaded = readImplementations(processingEnvironment, fileName, location);
-					if (location == StandardLocation.SOURCE_OUTPUT) existing = new TreeSet<>(loaded);
-					for (String implementation : loaded) {
-						final TypeElement element = elements.getTypeElement(implementation);
-						if (element != null) implementations.put(implementation, element);
-						else processingEnvironment.getMessager().printMessage(Diagnostic.Kind.WARNING, "Service implementation " + implementation + " does not exist, removing");
+				{
+					final Elements elements = processingEnvironment.getElementUtils();
+					Set<String> existing_ = null;
+					for (Location location : READ_LOCATIONS) {
+						final Set<String> loaded = readImplementations(processingEnvironment, fileName, location);
+						if (location == StandardLocation.SOURCE_OUTPUT) existing_ = new TreeSet<>(loaded);
+						for (String implementation : loaded) {
+							final TypeElement element = elements.getTypeElement(implementation);
+							if (element != null) implementations.put(implementation, element);
+							else processingEnvironment.getMessager().printMessage(Diagnostic.Kind.WARNING, "Service implementation " + implementation + " does not exist, removing");
+						}
 					}
+					existing = existing_;
 				}
 				
 				for (Element element : entry.getValue()) {
@@ -102,10 +105,8 @@ public class ServiceAnnotationHandler implements IAnnotationHandler<Service> {
 					implementations.put(typeElement.getQualifiedName().toString(), element);
 				}
 				
-				if (ENABLE_DEBUGGING) {
-					processingEnvironment.getMessager().printMessage(Diagnostic.Kind.WARNING, "Existing: " + existing.toString());
-					processingEnvironment.getMessager().printMessage(Diagnostic.Kind.WARNING, "Implementations: " + implementations.keySet());
-				}
+				debug(processingEnvironment, () -> "Existing: " + existing.toString());
+				debug(processingEnvironment, () -> "Implementations: " + implementations.keySet());
 				if (existing == null || !existing.equals(implementations.keySet())) {
 					writeImplementations(processingEnvironment, StandardLocation.CLASS_OUTPUT, fileName, implementations);
 				}
@@ -113,7 +114,9 @@ public class ServiceAnnotationHandler implements IAnnotationHandler<Service> {
 				final Messager messager = processingEnvironment.getMessager();
 				for (Element element : entry.getValue()) {
 					final String className = ((TypeElement) element).getQualifiedName().toString();
-					messager.printMessage(Diagnostic.Kind.ERROR, "Could not create resource \"" + fileName + " to add service \"" + className + "\"\": " + HAnnotationProcessor.toString(throwable), element);
+					final String message = "Could not create resource \"" + fileName + " to add service \"" + className + "\"\": " + HAnnotationProcessor.toString(throwable);
+					messager.printMessage(Diagnostic.Kind.ERROR, message, element);
+					debug(processingEnvironment, () -> message);
 				}
 			}
 		}
