4b0dda789f8ce31aa2e7e93f9cc6fd47a3099e2d
Greg Gibeling <gdgib@outlook.com>
2026-03-16 07:30:01 -0700

    G2-1769 Serdes factory interfaces

---
+++ ax-java/src/main/java/com/g2forge/alexandria/java/io/serdes/ISerdesFactoryRW.java
@@ -0,0 +1,3 @@
+package com.g2forge.alexandria.java.io.serdes;
+
+public interface ISerdesFactoryRW<T> extends ISerdesFactoryR_<T>, ISerdesFactory_W<T> {}
---
+++ ax-java/src/main/java/com/g2forge/alexandria/java/io/serdes/ISerdesFactory__.java
@@ -0,0 +1,3 @@
+package com.g2forge.alexandria.java.io.serdes;
+
+public interface ISerdesFactory__<T> {}
